b0fd4c454e40452c4cb8a4d8292cc4bb2aa33731
sakintoye2015 <sakintoye2015@my.fit.edu>
2016-09-14 16:10:49 -0400

    Migration for invitations and payments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,14 @@
 class ContactsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   respond_to :json
   def index
-    @contacts = {name: 'Ola', email: 'sakintoye@gmail.com'}
+    # @contacts = {name: 'Ola', email: 'sakintoye@gmail.com'}
     respond_to do |format|
+      # Contact.create({owner_id: 1, contact_id: 2})
+      @contacts = policy_scope(Contact.joins(:user))
+      @contacts.each do |contact|
+        print "Testing - #{contact.user.name}\n\n"
+      end
       format.json { render json: @contacts }
     end
   end

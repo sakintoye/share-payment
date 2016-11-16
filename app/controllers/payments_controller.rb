34cbca0518d1c34c9c4ff6f55027133c5de0f453
sakintoye <olasunkanmiakintoye@gmail.com>
2016-11-15 22:43:58 -0500

    Fixed issue with making payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -34,8 +34,7 @@ class PaymentsController < ApplicationController
   def create
     Stripe.api_key = Rails.application.secrets.stripe_key
     respond_to do |format|
-      payment_params = params[:payment]
-      payment = Payment.new(recipient_id: params[:recipient_id], amount: params[:amount], reason: params[:reason])
+      payment = Payment.new(create_params)
       recipient = User.find_by(id: payment.recipient_id)
       if recipient.present?
         begin
